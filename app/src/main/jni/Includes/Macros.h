e06f2029dda5d7e0bf3e9161d9d3dd82ce93202a
isPointer <158473007+ispointer@users.noreply.github.com>
2026-02-03 02:08:38 +0600

    Update Macros.h

--- app/src/main/jni/Includes/Macros.h
+++ app/src/main/jni/Includes/Macros.h
@@ -1,71 +1,133 @@
 #ifndef ANDROID_MOD_MENU_MACROS_H
 #define ANDROID_MOD_MENU_MACROS_H
 
-// thanks to shmoo and joeyjurjens for the usefull stuff under this comment.
+#include <dlfcn.h>
+#include <map>
+#include <string>
+#include <cstdint>
 
-#if defined(__aarch64__) //Compile for arm64 lib only
+
+#include "Logger.h"
+
+
+void hook(void *offset, void *ptr, void **orig) {
+#if defined(__aarch64__)
+    A64HookFunction(offset, ptr, orig);
+#else
+    MSHookFunction(offset, ptr, orig);
+#endif
+}
+
+
+#if defined(__aarch64__)
+
 #include <And64InlineHook/And64InlineHook.hpp>
 
-#define HOOK(offset, ptr, orig) A64HookFunction((void *)getAbsoluteAddress(targetLibName, string2Offset(OBFUSCATE(offset))), (void *)ptr, (void **)&orig)
-#define HOOK_LIB(lib, offset, ptr, orig) A64HookFunction((void *)getAbsoluteAddress(OBFUSCATE(lib), string2Offset(OBFUSCATE(offset))), (void *)ptr, (void **)&orig)
+#define HOOK(offset, ptr, orig) \
+A64HookFunction((void*)getAbsoluteAddress(targetLibName, string2Offset(OBFUSCATE(offset))), \
+                (void*)ptr, (void**)&orig)
 
-#define HOOK_NO_ORIG(offset, ptr) A64HookFunction((void *)getAbsoluteAddress(targetLibName, string2Offset(OBFUSCATE(offset))), (void *)ptr, NULL)
-#define HOOK_LIB_NO_ORIG(lib, offset, ptr) A64HookFunction((void *)getAbsoluteAddress(OBFUSCATE(lib), string2Offset(OBFUSCATE(offset))), (void *)ptr, NULL)
+#define HOOK_LIB(lib, offset, ptr, orig) \
+A64HookFunction((void*)getAbsoluteAddress(OBFUSCATE(lib), string2Offset(OBFUSCATE(offset))), \
+                (void*)ptr, (void**)&orig)
 
-#define HOOKSYM(sym, ptr, org) A64HookFunction(dlsym(dlopen(targetLibName, 4), OBFUSCATE(sym)), (void *)ptr, (void **)&org)
-#define HOOKSYM_LIB(lib, sym, ptr, org) A64HookFunction(dlsym(dlopen(OBFUSCATE(lib), 4), OBFUSCATE(sym)), (void *)ptr, (void **)&org)
+#define HOOK_NO_ORIG(offset, ptr) \
+A64HookFunction((void*)getAbsoluteAddress(targetLibName, string2Offset(OBFUSCATE(offset))), \
+                (void*)ptr, nullptr)
 
-#define HOOKSYM_NO_ORIG(sym, ptr) A64HookFunction(dlsym(dlopen(targetLibName, 4), OBFUSCATE(sym)), (void *)ptr, NULL)
-#define HOOKSYM_LIB_NO_ORIG(lib, sym, ptr) A64HookFunction(dlsym(dlopen(OBFUSCATE(lib), 4), OBFUSCATE(sym)), (void *)ptr, NULL)
+#define HOOKSYM(sym, ptr, orig) \
+A64HookFunction(dlsym(dlopen(targetLibName, RTLD_NOW), OBFUSCATE(sym)), \
+                (void*)ptr, (void**)&orig)
 
-#else //Compile for armv7 lib only. Do not worry about greyed out highlighting code, it still works
+#define HOOKSYM_NO_ORIG(sym, ptr) \
+A64HookFunction(dlsym(dlopen(targetLibName, RTLD_NOW), OBFUSCATE(sym)), \
+                (void*)ptr, nullptr)
 
+#else
 #include <Substrate/SubstrateHook.h>
 #include <Substrate/CydiaSubstrate.h>
 
-#define HOOK(offset, ptr, orig) MSHookFunction((void *)getAbsoluteAddress(targetLibName, string2Offset(OBFUSCATE(offset))), (void *)ptr, (void **)&orig)
-#define HOOK_LIB(lib, offset, ptr, orig) MSHookFunction((void *)getAbsoluteAddress(OBFUSCATE(lib), string2Offset(OBFUSCATE(offset))), (void *)ptr, (void **)&orig)
+#define HOOK(offset, ptr, orig) \
+MSHookFunction((void*)getAbsoluteAddress(targetLibName, string2Offset(OBFUSCATE(offset))), \
+               (void*)ptr, (void**)&orig)
 
-#define HOOK_NO_ORIG(offset, ptr) MSHookFunction((void *)getAbsoluteAddress(targetLibName, string2Offset(OBFUSCATE(offset))), (void *)ptr, NULL)
-#define HOOK_LIB_NO_ORIG(lib, offset, ptr) MSHookFunction((void *)getAbsoluteAddress(OBFUSCATE(lib), string2Offset(OBFUSCATE(offset))), (void *)ptr, NULL)
+#define HOOK_LIB(lib, offset, ptr, orig) \
+MSHookFunction((void*)getAbsoluteAddress(OBFUSCATE(lib), string2Offset(OBFUSCATE(offset))), \
+               (void*)ptr, (void**)&orig)
 
-#define HOOKSYM(sym, ptr, org) MSHookFunction(dlsym(dlopen(targetLibName, 4), OBFUSCATE(sym)), (void *)ptr, (void **)&org)
-#define HOOKSYM_LIB(lib, sym, ptr, org) MSHookFunction(dlsym(dlopen(OBFUSCATE(lib), 4), OBFUSCATE(sym)), (void *)ptr, (void **)&org)
+#define HOOK_NO_ORIG(offset, ptr) \
+MSHookFunction((void*)getAbsoluteAddress(targetLibName, string2Offset(OBFUSCATE(offset))), \
+               (void*)ptr, nullptr)
 
-#define HOOKSYM_NO_ORIG(sym, ptr)  MSHookFunction(dlsym(dlopen(targetLibName, 4), OBFUSCATE(sym)), (void *)ptr, NULL)
-#define HOOKSYM_LIB_NO_ORIG(lib, sym, ptr)  MSHookFunction(dlsym(dlopen(OBFUSCATE(lib), 4), OBFUSCATE(sym)), (void *)ptr, NULL)
+#define HOOKSYM(sym, ptr, orig) \
+MSHookFunction(dlsym(dlopen(targetLibName, RTLD_NOW), OBFUSCATE(sym)), \
+               (void*)ptr, (void**)&orig)
+
+#define HOOKSYM_NO_ORIG(sym, ptr) \
+MSHookFunction(dlsym(dlopen(targetLibName, RTLD_NOW), OBFUSCATE(sym)), \
+               (void*)ptr, nullptr)
 
 #endif
 
-// Obfuscate offset
-#define OBFUSCATEOFFSET(str) string2Offset(OBFUSCATE(str)) // Encrypt offset
 
-// Patching a offset without switch.
-void patchOffset(const char *fileName, uint64_t offset, std::string hexBytes) {
-    MemoryPatch patch = MemoryPatch::createWithHex(fileName, offset, hexBytes);
-    if(!patch.isValid()){
-        LOGE(OBFUSCATE("Failing offset: 0x%llu, please re-check the hex you entered."), offset);
+#define OBFUSCATEOFFSET(str) string2Offset(OBFUSCATE(str))
+
+
+inline void patchOffset(const char *lib, uint64_t offset, const std::string &hex) {
+    MemoryPatch patch = MemoryPatch::createWithHex(lib, offset, hex);
+    if (!patch.isValid()) {
+        LOGE("Invalid patch offset: 0x%llX", offset);
         return;
     }
-    if(!patch.Modify()) {
-        LOGE(OBFUSCATE("Something went wrong while patching this offset: 0x%llu"), offset);
+    patch.Modify();
+}
+
+inline void patchOffset(uint64_t offset, const std::string &hex) {
+    patchOffset(targetLibName, offset, hex);
+}
+
+#define PATCHOFFSET(offset, hex) \
+patchOffset(string2Offset(OBFUSCATE(offset)), OBFUSCATE(hex))
+
+#define PATCHOFFSET_LIB(lib, offset, hex) \
+patchOffset(OBFUSCATE(lib), string2Offset(OBFUSCATE(offset)), OBFUSCATE(hex))
+
+
+inline void patchOffsetSym(uintptr_t addr,
+                           const std::string &hex,
+                           bool enable) {
+    static std::map<uintptr_t, MemoryPatch> patchMap;
+
+    if (!addr) {
+        LOGE("patchOffsetSym: NULL address");
         return;
+    }
+
+    if (!patchMap.count(addr)) {
+        patchMap[addr] = MemoryPatch::createWithHex(addr, hex);
+    }
+
+    MemoryPatch &patch = patchMap[addr];
+
+    if (!patch.isValid()) {
+        LOGE("patchOffsetSym: invalid patch at %p", (void*)addr);
+        return;
+    }
+
+    if (enable) {
+        patch.Modify();
+    } else {
+        patch.Restore();
     }
 }
 
-void patchOffset(uint64_t offset, std::string hexBytes) {
-    MemoryPatch patch = MemoryPatch::createWithHex(targetLibName, offset, hexBytes);
-    if(!patch.isValid()){
-        LOGE(OBFUSCATE("Failing offset: 0x%llu, please re-check the hex you entered."), offset);
-        return;
-    }
-    if(!patch.Modify()) {
-        LOGE(OBFUSCATE("Something went wrong while patching this offset: 0x%llu"), offset);
-        return;
-    }
-}
 
-#define PATCHOFFSET(offset, hex) patchOffset(string2Offset(OBFUSCATE(offset)), OBFUSCATE(hex))
-#define PATCHOFFSET_LIB(lib, offset, hex) patchOffset(OBFUSCATE(lib), string2Offset(OBFUSCATE(offset)), OBFUSCATE(hex))
+#define PATCH_SYM_SWITCH(sym, hex, state) \
+patchOffsetSym((uintptr_t)dlsym(dlopen(targetLibName, RTLD_NOW), OBFUSCATE(sym)), \
+               OBFUSCATE(hex), state)
 
-#endif //ANDROID_MOD_MENU_MACROS_H
+#define PATCH_LIB_SYM_SWITCH(lib, sym, hex, state) \
+patchOffsetSym((uintptr_t)dlsym(dlopen(OBFUSCATE(lib), RTLD_NOW), OBFUSCATE(sym)), \
+               OBFUSCATE(hex), state)
+
+#endif // ANDROID_MOD_MENU_MACROS_H
